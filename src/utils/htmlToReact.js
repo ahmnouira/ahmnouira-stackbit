b18b3f22ef012c165693e22fdb8c2001c64bb699
Ahmed Nouria <ahmnouira@gmail.com>
2023-10-08 17:57:44 +0100

    updates

--- src/utils/htmlToReact.js
+++ src/utils/htmlToReact.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser';
-import ScriptTag from 'react-script-tag';
-import Link from './link';
-import _ from 'lodash';
-
-const convertChildren = (children, index) => _.map(children, childNode => convertNodeToElement(childNode, index, _.noop()));
+import HtmlToReact, { Parser as HtmlToReactParser } from "html-to-react";
 
 export default function htmlToReact(html) {
-    if (!html) {
-        return null;
-    }
-    return ReactHtmlParser(html, {
-        transform: (node, index) => {
-            if (node.type === 'script') {
-                if (!_.isEmpty(node.children)) {
-                    return (
-                        <ScriptTag key={index} {...node.attribs}>
-                            {convertChildren(node.children, index)}
-                        </ScriptTag>
-                    );
-                } else {
-                    return <ScriptTag key={index} {...node.attribs}/>;
-                }
-            } else if (node.type === 'tag' && node.name === 'a') {
-                const href = node.attribs.href;
-                const props = _.omit(node.attribs, 'href');
-                // use Link only if there are no custom attributes like style, class, and what's not that might break react
-                if (_.isEmpty(props)) {
-                    return <Link key={index} to={href} {...props}>{convertChildren(node.children, index)}</Link>;
-                }
-            }
-        }
-    });
-};
+  if (!html) {
+    return null;
+  }
+
+  const isValidNode = function () {
+    return true;
+  };
+
+  // Order matters. Instructions are processed in the order they're defined
+  const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions();
+  const processingInstructions = [
+    {
+      // Custom <h1> processing
+      shouldProcessNode: function (node) {
+        return node.parent && node.parent.name && node.parent.name === "h1";
+      },
+      processNode: function (node, children) {
+        return node.data.toUpperCase();
+      },
+    },
+    {
+      // Anything else
+      shouldProcessNode: function (node) {
+        return true;
+      },
+      processNode: processNodeDefinitions.processDefaultNode,
+    },
+  ];
+
+  const htmlToReactParser = new HtmlToReactParser();
+
+  return htmlToReactParser.parseWithInstructions(
+    html,
+    isValidNode,
+    processingInstructions
+  );
+}
--- src/utils/markdownify.js
+++ src/utils/markdownify.js
@@ -1,9 +1,9 @@
-import marked from "marked";
+import { marked } from "marked";
 import htmlToReact from "./htmlToReact";
 
 export default function markdownify(markdown) {
   if (!markdown) {
     return null;
   }
-  return htmlToReact(marked(markdown));
+  return htmlToReact(markdown);
 }
